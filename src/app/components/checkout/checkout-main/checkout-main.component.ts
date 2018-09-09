821cfa94280fef8f5a8d6a4b27e82fdfc23aefbf
jontysouvik <jontysouvik@gmail.com>
2018-09-10 00:04:16 +0530

    Check out started

--- src/app/components/checkout/checkout-main/checkout-main.component.ts
+++ src/app/components/checkout/checkout-main/checkout-main.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Address } from '../../../models/address';
+import { Subscription } from 'rxjs';
+import { UserService } from '../../../services/user.service';
+import { User } from '../../../models/user';
 
 @Component({
   selector: 'app-checkout-main',
@@ -6,10 +10,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout-main.component.css']
 })
 export class CheckoutMainComponent implements OnInit {
-
-  constructor() { }
+  addresses: Address[] = [];
+  userSubscription: Subscription;
+  constructor(private userSvc: UserService) { }
 
   ngOnInit() {
+    this.addresses = this.userSvc.userDetails.addresses
+    this.selectDefaultAddress();
+    this.userSubscription = this.userSvc.userDataEventEmmiter.subscribe((res: User) => {
+      this.addresses = res.addresses;
+      this.selectDefaultAddress();
+    });
   }
-
+  selectDefaultAddress() {
+    for (let index = 0; index < this.addresses.length; index++) {
+      const address: any = this.addresses[index];
+      if (address.isDefault) {
+        address.isSelected = true;
+      }
+    }
+  }
+  onAddressSelect(selectedAddress) {
+    for (let index = 0; index < this.addresses.length; index++) {
+      const address: any = this.addresses[index];
+      if (selectedAddress.id === address.id) {
+        address.isSelected = true;
+      } else {
+        address.isSelected = false; 
+      }
+    }
+  }
 }
